101c285c4facde4c9accab80f1ded383b4e6f0aa
Gintamafan25 <130246102+Gintamafan25@users.noreply.github.com>
2026-05-22 21:40:27 +0000

    working on list

--- app.py
+++ app.py
@@ -1,10 +1,31 @@
-from flask import Flask,render_template
+from flask import Flask,render_template, jsonify
+import requests
 
 app = Flask(__name__)
 
+url = 'https://api.freeapi.app/api/v1/public/books?page=1&limit=10&inc=kind%2Cid%2Cetag%2CvolumeInfo&query=tech'
+url2 = "https://dashboard.elering.ee/api/nps/price/EE/current"
+url3 = "https://official-joke-api.appspot.com/random_joke"
+
+url_list = [url,url2,url3]
 @app.route("/")
 def index():
     return render_template("index.html")
 
+@app.route("/api/books")
+def get_books():
+   
+    response = requests.get(url2)
+
+    if response.status_code != 200:
+        print(response.status_code)
+        print(response.text)
+        return jsonify({"error": "API failed"}), 500
+    data = response.json()
+    return jsonify(data)
+
+@app.route("/api/list")
+def get_list():
+    return jsonify(url_list)
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
--- static/script.js
+++ static/script.js
@@ -1,31 +1,70 @@
 document.addEventListener("DOMContentLoaded", async ()=>{
     let dataSetOne = await loadData();
-    console.log(dataSetOne);
+    let url_list = await getList();
     
+    
+    let testData = {"tag": "rankings", "rated-ranks": [98,97,94], "number of raters" : 24 }
+   
     let masterList = []
-
+    masterLoader(masterList,dataSetOne)
     
+   
+   
 })
+
 const url = 'https://api.freeapi.app/api/v1/public/books?page=1&limit=10&inc=kind%2Cid%2Cetag%2CvolumeInfo&query=tech'
+const url2 = "api/books"
 
 function masterLoader(masterList,data) {
     masterList.length = 0;
-    data.foreach((a) =>{
-        console.log(a)
-    })
-}
+   
 
-function treeWalker(masterList,node) {
-    if (!node.isArray() && node !== null && typeof node === object) {
-        masterList.push({dataType: typeof node, layer: })
+   
+    const layer = 0
+    masterList.push({dataType:typeof data, layer: layer, value : data, parent: null})
+    let index = masterList.findIndex(item => item.value === data)
+    
+    for (let x in data) {
+        treeWalker(masterList,data[x], layer, masterList[index])
     }
+    
 }
 
+function treeWalker(masterList,node, layer, parent) {
+    
 
+    if (typeof node !== "object" ) {
+        
+        masterList.push({dataType : typeof node, layer : layer + 1, value : node, parent : parent})
+        return
+    } else {
+        try {
+            
+            masterList.push({dataType: typeof node, layer : layer + 1, value: node, parent : parent})
+            let index = masterList.findIndex(item => item.value === node)
+            for (let y in node) {
+                treeWalker(masterList, node[y], layer + 1, masterList[index])
+            }
+            
+        } catch (e) {
+            console.log(e, "error")
+        }
+    }
+
+    return;
+}
+
+function displayList(list) {
+    const table = document.createElement("table");
+    const tableHeaderRow = document.createElement("tr")
+    const tableHeader = document.createElement("th")
+
+    
+}
 
 async function loadData() {
     try {
-        let response = await fetch(url);
+        let response = await fetch(url2);
         if (!response.ok) {
             console.log("HTTP Error:", response.status);
             return [];
@@ -39,4 +78,19 @@ async function loadData() {
         return [];
     }
     
+}
+
+async function getList() {
+    try {
+        let response = await fetch("/api/list");
+        if (!response.ok) {
+            console.log("HTTP Error:", response.status);
+            return []
+        }
+
+        return await response.json();
+    } catch (e) {
+        console.log("failed to load url lists", e)
+        return [];
+    }
 }
